62c89a1a3f69ae68a1472c4b8fda1e42bdf90d42
Gustavo Colombo Rocha <gusttavocolombo@gmail.com>
2023-02-23 22:37:33 -0300

    chore:update icon empty list

--- todo-list-ignite/src/components/EmptyList/EmptyList.tsx
+++ todo-list-ignite/src/components/EmptyList/EmptyList.tsx
@@ -1,12 +1,12 @@
-import { ClipboardText } from 'phosphor-react-native';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import { styles } from './styles';
+import list from '../../assets/list.png';
 
 export function EmptyList() {
   return (
     <View style={styles.container}>
-      <ClipboardText size={100} color="#808080" />
+      <Image source={list} style={{ height: 65, width: 65, marginBottom: 16 }} />
       <View style={styles.containerText}>
         <Text style={styles.textBold}>Você ainda não tem tarefas cadastradas</Text>
         <Text style={styles.textNormal}>Crie tarefas e organize seus itens a fazer</Text>
--- todo-list-ignite/src/components/Header/Header.tsx
+++ todo-list-ignite/src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
-import { Rocket } from 'phosphor-react-native';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { styles } from './styles';
+import rocket from '../../assets/rocket.png';
 
 export function Header() {
   return (
     <View style={styles.container}>
-      <Rocket size={30} weight="bold" color="#fff" />
+      <Image source={rocket} />
       <View style={styles.todoStyle}>
         <Text style={styles.textBlue}>to</Text>
         <Text style={styles.textPurple}>do</Text>
